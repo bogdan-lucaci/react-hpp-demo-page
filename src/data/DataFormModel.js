6f6957b6906145cc2d4d0b1632016c1cc4394d2d
bogdan.lucaci <bogdan.lucaci@smart2pay.com>
2021-03-07 12:45:46 +0200

    updated FormDataModel

--- src/data/DataFormModel.js
+++ src/data/DataFormModel.js
@@ -1,35 +1,183 @@
 let formModel = {
+    areas: [
+        { id: 1, name: "Mandatory" },
+        { id: 2, name: "Flow Control" },
+        { id: 3, name: "Optional" },
+        {
+            id: 4, name: "Method Specific", subArea: [
+                { id: 1, name: "Alipay" },
+                { id: 2, name: "Cards" },
+                { id: 3, name: "DLocal - India" },
+                { id: 4, name: "Klarna" },
+                { id: 5, name: "Todito Cash" },
+                { id: 6, name: "Union Pay" },
+            ]
+        },
+        { id: 5, name: "Payouts" },
+        {
+            id: 6, name: "Recurrent Payments", subArea: [
+                { id: 1, name: "SlimPay" },
+                { id: 2, name: "Skrill" },
+                { id: 3, name: "Pay by mobile (Fortumo)" },
+            ]
+        },
+        {
+            id: 7, name: "Refunds", subArea: [
+                { id: 1, name: "Extra Parameters" },
+                { id: 2, name: "Manual Support Work Details Needed" }
+            ]
+        },
+        { id: 8, name: "Capture Payments" },
+        { id: 9, name: "Cancel Payments" }
+    ],
     params: [
-        {name: "Post URL", isPaymentParam: false},
-        {name: "MerchantID", isPaymentParam: true},
-        {name: "Amount", isPaymentParam: true},
-        {name: "Currency", isPaymentParam: true},
-        {name: "ReturnURL", isPaymentParam: true},
-        {name: "Country", isPaymentParam: true},
-        {name: "Hash", isPaymentParam: true},
-        {name: "ComputedString", isPaymentParam: true},
-        {name: "MethodID", isPaymentParam: true},
-        {name: "CustomerEmail", isPaymentParam: true},
-        {name: "ReferenceNumber", isPaymentParam: true},
-        {name: "CustomerName", isPaymentParam: true},
-        {name: "CustomerPhone", isPaymentParam: true},
-        {name: "Description", isPaymentParam: true},
-        {name: "SkipHPP", isPaymentParam: true},
-        {name: "RedirectInIframe", isPaymentParam: true},
-        {name: "MerchantRedirectInIframe", isPaymentParam: true},
-        {name: "DisplayInsideIframe", isPaymentParam: false},
-        {name: "IframeOverlaySource", isPaymentParam: false},
-        
-        
+        //{name: "Post URL", isPaymentParam: false, area: [1]},
+        // Mandatory
+        { name: "MerchantID", isPaymentParam: true, area: [1] },
+        { name: "Amount", isPaymentParam: true, area: [1] },
+        { name: "Currency", isPaymentParam: true, area: [1] },
+        { name: "ReturnURL", isPaymentParam: true, area: [1] },
+        { name: "Country", isPaymentParam: true, area: [1] },
+        { name: "Hash", isPaymentParam: true, area: [1] },
+        { name: "ComputedString", isPaymentParam: false, area: [1] },
+
+        // Flow Control
+        { name: "MethodID", isPaymentParam: true, area: [2] },
+        { name: "CustomerEmail", isPaymentParam: true, area: [2] },
+        { name: "ReferenceNumber", isPaymentParam: true, area: [2] },
+        { name: "CustomerName", isPaymentParam: true, area: [2] },
+        { name: "CustomerPhone", isPaymentParam: true, area: [2] },
+        { name: "Description", isPaymentParam: true, area: [2] },
+        { name: "SkipHPP", isPaymentParam: true, area: [2] },
+        { name: "RedirectInIframe", isPaymentParam: true, area: [2] },
+        { name: "MerchantRedirectInIframe", isPaymentParam: true, area: [2] },
+        { name: "DisplayInsideIframe", isPaymentParam: false, area: [2] },
+        { name: "IframeOverlaySource", isPaymentParam: false, area: [2] },
+
+        // Optional        
+        { name: "SiteID", isPaymentParam: true, area: [3] },
+        { name: "SkinID", isPaymentParam: true, area: [3] },
+        { name: "IncludeMethodIDs", isPaymentParam: true, area: [3] },
+        { name: "ExcludeMethodIDs", isPaymentParam: true, area: [3] },
+        { name: "PrioritizeMethodIDs", isPaymentParam: true, area: [3] },
+        { name: "Guaranteed", isPaymentParam: true, area: [3] },
+        { name: "Language", isPaymentParam: true, area: [3] },
+        { name: "OriginatorTransactionID", isPaymentParam: true, area: [3] },
+
+        // Method Specific
+        { name: "MethodOptionID", isPaymentParam: true, area: [4] },
+        { name: "PayerAmount", isPaymentParam: true, area: [4] },
+        { name: "Street", isPaymentParam: true, area: [4] },
+        { name: "StreetNumber", isPaymentParam: true, area: [4] },
+        { name: "ZipCode", isPaymentParam: true, area: [4] },
+        { name: "City", isPaymentParam: true, area: [4] },
+        { name: "State", isPaymentParam: true, area: [4] },
+        { name: "CustomerAddress", isPaymentParam: true, area: [4] },
+        { name: "AccountNumber", isPaymentParam: true, area: [4] },
+        { name: "IBAN", isPaymentParam: true, area: [4] },
+        { name: "BIC", isPaymentParam: true, area: [4] },
+        { name: "Company", isPaymentParam: true, area: [4] },
+        { name: "CustomerSocialSecurityNumber", isPaymentParam: true, area: [4] },
+        { name: "Articles", isPaymentParam: true, area: [4] },
+        // Method Specific / Alipay
+        { name: "IsOffline", isPaymentParam: true, area: [4, 1] },
+        { name: "StoreName", isPaymentParam: true, area: [4, 1] },
+        { name: "StoreID", isPaymentParam: true, area: [4, 1] },
+        { name: "TerminalID", isPaymentParam: true, area: [4, 1] },
+        // Method Specific / Cards
+        { name: "Installments", isPaymentParam: true, area: [4, 2] },
+        { name: "CardHolderName", isPaymentParam: true, area: [4, 2] },
+        { name: "CardType", isPaymentParam: true, area: [4, 2] },
+        { name: "ExpirationMonth", isPaymentParam: true, area: [4, 2] },
+        { name: "ExpirationYear", isPaymentParam: true, area: [4, 2] },
+        { name: "SecurityCode", isPaymentParam: true, area: [4, 2] },
+        { name: "CustomerCPF", isPaymentParam: true, area: [4, 2] },
+        { name: "CustomerZipCode", isPaymentParam: true, area: [4, 2] },
+        // Method Specific / DLocal - India
+        { name: "Address", isPaymentParam: true, area: [4, 3] },
+        // Method Specific / Klarna
+        { name: "CustomerFirstName", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerLastName", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerGender", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerDateOfBirth", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerBillingAddress", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerShippingAddress", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerAccountNumber", isPaymentParam: true, area: [4, 4] },
+        { name: "CustomerBankName", isPaymentParam: true, area: [4, 4] },
+        // Method Specific / Todito Cash
+        { name: "CardNumber", isPaymentParam: true, area: [4, 5] },
+        { name: "CardPIN", isPaymentParam: true, area: [4, 5] },
+        // Method Specific / Union Pay
+        { name: "Capture", isPaymentParam: true, area: [4, 6] },
+
+        // Payouts
+        { name: "CustomerBankAccountID", isPaymentParam: true, area: [5] },
+        { name: "CryptoAddress", isPaymentParam: true, area: [5] },
+        { name: "CryptoCurrency", isPaymentParam: true, area: [5] },
+        { name: "ActionName", isPaymentParam: true, area: [5] },
+
+        // Recurrent Payments
+        { name: "ActionName", isPaymentParam: true, area: [6] },
+        { name: "MerchantPreapprovalID", isPaymentParam: true, area: [6] },
+        { name: "PreapprovalDescription", isPaymentParam: true, area: [6] },
+        { name: "PreapprovalID", isPaymentParam: true, area: [6] },
+        { name: "PreapprovalReturnURL", isPaymentParam: true, area: [6] },
+        // Recurrent Payments / Slimpay
+        { name: "FirstName", isPaymentParam: true, area: [6, 1] },
+        { name: "LastName", isPaymentParam: true, area: [6, 1] },
+        { name: "Email", isPaymentParam: true, area: [6, 1] },
+        { name: "Phone", isPaymentParam: true, area: [6, 1] },
+        { name: "ExecutionDate", isPaymentParam: true, area: [6, 1] },
+        // Recurrent Payments / Skrill
+        { name: "PreapprovedMaximumAmount", isPaymentParam: true, area: [6, 2] },
+        // Recurrent Payments / Pay by mobile (Fortumo)
+        { name: "PreapprovedFrequency", isPaymentParam: true, area: [6, 3] },
+
+        // Refunds
+        { name: "ActionName", isPaymentParam: true, area: [7] },
+        { name: "InitialPaymentID", isPaymentParam: true, area: [7] },
+        // Refunds / Extra Parameters
+        { name: "InitialPaymentID", isPaymentParam: true, area: [7, 1] },
+        { name: "BankAccountType", isPaymentParam: true, area: [7, 1] },
+        { name: "BankAddress", isPaymentParam: true, area: [7, 1] },
+        { name: "BankAgencyCode", isPaymentParam: true, area: [7, 1] },
+        { name: "BankCode", isPaymentParam: true, area: [7, 1] },
+        { name: "BankCountry", isPaymentParam: true, area: [7, 1] },
+        { name: "BankName", isPaymentParam: true, area: [7, 1] },
+        { name: "BankSortCode", isPaymentParam: true, area: [7, 1] },
+        { name: "SWIFTcode", isPaymentParam: true, area: [7, 1] },
+        { name: "BankSWIFTID", isPaymentParam: true, area: [7, 1] },
+        { name: "CPFAccountHolder", isPaymentParam: true, area: [7, 1] },
+        { name: "CustomerAccountNumber", isPaymentParam: true, area: [7, 1] },
+        { name: "CustomerCity", isPaymentParam: true, area: [7, 1] },
+        { name: "CustomerCountry", isPaymentParam: true, area: [7, 1] },
+        { name: "CustomerIBAN", isPaymentParam: true, area: [7, 1] },
+        // Refunds / Manual Support Work Details Needed
+        { name: "THBankAccountNumber", isPaymentParam: true, area: [7, 2] },
+        { name: "BankBranch", isPaymentParam: true, area: [7, 2] },
+        { name: "AccountNoIDR", isPaymentParam: true, area: [7, 2] },
+
+        // Capture Payments
+        { name: "ActionName", isPaymentParam: true, area: [8] },
+        { name: "InitialPaymentID", isPaymentParam: true, area: [8] },
+
+        // Cancel Payments
+        { name: "ActionName", isPaymentParam: true, area: [9] },
+        { name: "InitialPaymentID", isPaymentParam: true, area: [9] },
     ],
     helpers: [
-        {for: "MerchantID"},
-        {for: "Currency"},
-        {for: "Country"},
-        {for: "MethodID"},
-        {for: "SkipHPP"},
-        {for: "RedirectInIframe"},
-        {for: "MerchantRedirectInIframe"},
+        { for: "MerchantID" },
+        { for: "Currency" },
+        { for: "Country" },
+        { for: "MethodID" },
+        { for: "SkipHPP" },
+        { for: "RedirectInIframe" },
+        { for: "MerchantRedirectInIframe" },
+        { for: "SiteID" },
+        { for: "Articles" },
+        { for: "IsOffline" },
+        { for: "Capture" },
+        { for: "ActionName" },
     ]
 };
 
